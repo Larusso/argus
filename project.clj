aa39c85c573455ced2105bc51d92da70b4b427c2
Manfred Endres <manfred.endres@tslarusso.de>
2013-11-10 23:09:18 +0100

    enable logging set project settings for uberjar creation

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [uochan/watchtower "0.1.4"]
-                 [aleph "0.3.0"]]
+                 [aleph "0.3.0"]
+                 [com.taoensso/timbre "2.7.1"]]
   :main pearl-file-watcher.core
+  :aot [pearl-file-watcher.core]
   :jvm-opts ["-Djava.net.preferIPv4Stack=true"])
--- src/pearl_file_watcher/core.clj
+++ src/pearl_file_watcher/core.clj
@@ -2,20 +2,35 @@
   (:require [watchtower.core :refer :all]
             [lamina.core :refer :all]
             [aleph.tcp :refer :all]
-            [gloss.core :refer :all]))
+            [gloss.core :refer :all]
+            [taoensso.timbre :as timbre :refer (trace debug info warn fatal spy with-log-level)])
+  (:gen-class))
 
 (def main-channel (channel))
 (def server-port 9000)
 
 (defn files-changed [files]
+  (info "files changed")
+  (spy :info files)
   (doall
-   (map #(enqueue main-channel (.getAbsolutePath %)) files)))
+   (map #(enqueue main-channel (.getPath %)) files)))
 
 (defn handler [channel client-info]
+  (info "channel connected")
   (siphon main-channel channel))
 
+(def log-file (str (System/getenv "HOME") "/Library/Logs/pearl_watcher.log"))
 
-(defn -main []
+(defn initLogger
+  []
+  (timbre/set-config! [:appenders :spit :enabled?] true)
+  (timbre/set-config! [:shared-appender-config :spit-filename] log-file))
+
+
+(defn -main
+  [& args]
+  (initLogger)
+  (info "start watcher")
   (start-tcp-server handler {:port server-port, :frame (string :utf-8 :delimiters ["\r\n"])})
   (watcher ["resources/"]
            (rate 200)
