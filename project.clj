ad3bceed14b44390ba36ff1f8369ec4fc014d908
Manfred Endres <manfred.endres@tslarusso.de>
2013-11-11 10:04:57 +0100

    implement cli options

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [uochan/watchtower "0.1.4"]
                  [aleph "0.3.0"]
-                 [com.taoensso/timbre "2.7.1"]]
+                 [com.taoensso/timbre "2.7.1"]
+                 [org.clojure/tools.cli "0.2.4"]]
   :main pearl-file-watcher.core
   :aot [pearl-file-watcher.core]
   :jvm-opts ["-Djava.net.preferIPv4Stack=true"])
--- src/pearl_file_watcher/core.clj
+++ src/pearl_file_watcher/core.clj
@@ -3,6 +3,7 @@
             [lamina.core :refer :all]
             [aleph.tcp :refer :all]
             [gloss.core :refer :all]
+            [clojure.tools.cli :refer (cli)]
             [taoensso.timbre :as timbre :refer (trace debug info warn fatal spy with-log-level)])
   (:gen-class))
 
@@ -19,22 +20,38 @@
   (info "channel connected")
   (siphon main-channel channel))
 
-(def log-file (str (System/getenv "HOME") "/Library/Logs/pearl_watcher.log"))
-
 (defn initLogger
-  []
+  [path]
   (timbre/set-config! [:appenders :spit :enabled?] true)
-  (timbre/set-config! [:shared-appender-config :spit-filename] log-file))
+  (timbre/set-config! [:shared-appender-config :spit-filename] path))
+
+(defn init-watcher
+  [{host :host
+    port :port
+    check-rate :rate :as p}]
+  (info "start watcher")
+  (spy :debug p)
+  (start-tcp-server handler {:host host :port port, :frame (string :utf-8 :delimiters ["\r\n"])})
+  (watcher ["resources/"]
+           (rate check-rate)
+           (file-filter ignore-dotfiles)
+           (file-filter (extensions :json))
+           (on-change files-changed)))
 
 
 (defn -main
   [& args]
-  (initLogger)
-  (info "start watcher")
-  (start-tcp-server handler {:port server-port, :frame (string :utf-8 :delimiters ["\r\n"])})
-  (watcher ["resources/"]
-           (rate 200)
-           (file-filter ignore-dotfiles)
-           (file-filter (extensions :json))
-           (on-change files-changed)))
+  (let [[options args banner] (cli args "pearl file watcher"
+    ["--host" "The hostname" :default "localhost"]
+    ["-p" "--port" "Listen on this port" :parse-fn #(Integer. %) :default 9000]
+    ["-r" "--rate" "Delay between samples" :parse-fn #(Integer. %) :default 100]
+    ["-h" "--help" "Show help" :default false :flag true]
+    ["--log" "path for log output" :default (str (System/getenv "HOME") "/Library/Logs/pearl_watcher.log")])
+        usage (clojure.string/replace banner #"Usage:" "usage: modler [options] [model PATH]")]
+
+    (when (:help options)
+      (println banner)
+      (System/exit 0))
+    (initLogger (:log options))
+    (init-watcher options)))
 
